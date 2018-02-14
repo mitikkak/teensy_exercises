7c72bfef235946350ee584d6c4bc6434baa0938f
Mikko Tikkakoski <mikko.tikkakoski@nokia.com>
2018-02-14 17:29:01 +0200

    core library built. Linking fails.

--- blink/Loop.cpp
+++ blink/Loop.cpp
@@ -1,7 +1,13 @@
 
 #include "Arduino.h"
 
+static const int ledPin = 2;
 void loop()
 {
-
+    digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+    //_SERIAL.println("HIGH");
+    delay(1000);                       // wait for a second
+    digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+    //_SERIAL.println("LOW");
+    delay(1000);                       // wait for a second
 }
--- blink/Setup.cpp
+++ blink/Setup.cpp
@@ -1,6 +1,7 @@
 #include "Arduino.h"
 
+static const int ledPin = 2;
 void setup()
 {
-
+  pinMode(ledPin, OUTPUT);
 }
