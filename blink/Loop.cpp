21a37a9807849f356bcbc7140555379766ec1911
Mikko <mikko.tikkakoski@gmail.com>
2018-02-15 20:46:08 +0200

    upgraded to arduino 1.8.5

--- blink/Loop.cpp
+++ blink/Loop.cpp
@@ -1,13 +1,12 @@
 
 #include "Arduino.h"
 
-static const int ledPin = 2;
 void loop()
 {
-    digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
     //_SERIAL.println("HIGH");
-    delay(1000);                       // wait for a second
-    digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+    delay(2000);                       // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
     //_SERIAL.println("LOW");
-    delay(1000);                       // wait for a second
+    delay(2000);                       // wait for a second
 }
--- blink/Setup.cpp
+++ blink/Setup.cpp
@@ -1,7 +1,6 @@
 #include "Arduino.h"
 
-static const int ledPin = 2;
 void setup()
 {
-  pinMode(ledPin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
