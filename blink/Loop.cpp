95d7e58e2d9c128086a3100c7afd4bf4956775a8
Mikko <mikko.tikkakoski@gmail.com>
2018-02-16 20:16:51 +0200

    blink now fully works

--- blink/Loop.cpp
+++ blink/Loop.cpp
@@ -5,8 +5,8 @@ void loop()
 {
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
     //_SERIAL.println("HIGH");
-    delay(2000);                       // wait for a second
+    delay(500);                       // wait for a second
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
     //_SERIAL.println("LOW");
-    delay(2000);                       // wait for a second
+    delay(500);                       // wait for a second
 }
